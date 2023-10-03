8b6a913802c04d7e63ff1bd614cefffa356d7432
Cosmotxt <reni91_@outlook.com>
2023-10-03 10:53:18 -0300

    commit

--- client/pages/cardtips.tsx
+++ client/pages/cardtips.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CardTips = ({ text, onClick } : any) => {
     return (
-        <div className="p-[2vh] w-[15vw] h-[10vh] rounded-[3vw] border text-center cursor-pointer mx-auto max-lg:w-[90%]" onClick={onClick}>
+        <div className="p-[2vh] w-[15vw] h-[10vh] rounded-[3vw] border text-center cursor-pointer mx-auto max-lg:w-[90%] max-lg:h-full" onClick={onClick}>
             <span className="flex flex-wrap h-full items-center justify-center text-neutral-300 text-sm">
                 {text}  
             </span>
